6f6f23608c160b48d9c73934a195c92d0485ee61
mihir-n25 <mihirnebani@gmail.com>
2024-06-03 11:11:35 +0530

    some changes

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -21,9 +21,35 @@ export const ConfirmModal = ({
     children, 
     onConfirm
 } : ConfirmModalProps) => {
+    const handleConfirm = (
+        e : React.MouseEvent<HTMLButtonElement , MouseEvent>
+    ) => {
+        e.stopPropagation(),
+        onConfirm()
+    }
     return(
         <AlertDialog>
-            <AlertDialogTrigger
+            <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                <AlertDialogTitle>
+                    Are You Sure?
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone.
+                </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction onClick={handleConfirm}>
+                        Confirm
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
         </AlertDialog>
     )
 }

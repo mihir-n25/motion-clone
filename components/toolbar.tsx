fedaa8107473bb57cbc2c8b8ae99e27cc5bc68fe
mihir-n25 <mihirnebani@gmail.com>
2024-06-26 19:08:05 +0530

    icon added success

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Button } from "@/components/ui/button";
+import { Smile, X } from "lucide-react";
 
 interface ToolbarProps {
 initialData : Doc<"documents">;
@@ -14,7 +17,48 @@ export const Toolbar = ({
     return(
         <div
          className="pl-[54px] group relative">
-            Toolbar
+            {
+                !!initialData.icon && !preview && (
+                    <div className="flex items-center gap-x-2 group/icon pt-6">
+                        <IconPicker onChange={() => {}}>
+                            <p className="text-6xl hover:opacity-75 transition">
+                                {initialData.icon}
+                            </p>
+                        </IconPicker>
+                       <Button
+                       onClick={() => {}}
+                       className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+                       variant={"outline"}
+                       size="icon"
+                       >
+                        <X className="h-4 w-4"/>
+                       </Button>
+                    </div>
+                )
+            }
+            {
+                !!initialData.icon && preview && (
+                    <p className="text-6xl pt-6">
+                        {initialData.icon}
+                    </p>
+                )
+            }
+            <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+                {
+                    !initialData.icon && !preview && (
+                        <IconPicker asChild onChange={() => {}}>
+                            <Button
+                            className="text-muted-foreground text-xs"
+                            variant="outline"
+                            size="sm"
+                            >
+                                <Smile className="h-4 w-4 mr-2"/>
+                                Add Icon
+                            </Button>
+                        </IconPicker>
+                    )
+                }
+            </div>
         </div>
     )
 }

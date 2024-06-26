6068fb427bfc03338dde5876e2927cdf90c00cbf
mihir-n25 <mihirnebani@gmail.com>
2024-06-26 19:32:48 +0530

    toolbar finished

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+import TextareaAutoSize from "react-textarea-autosize"
+
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
 import { Button } from "@/components/ui/button";
-import { Smile, X } from "lucide-react";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 interface ToolbarProps {
 initialData : Doc<"documents">;
@@ -14,6 +19,41 @@ export const Toolbar = ({
     initialData,
     preview
 } : ToolbarProps) => {
+
+    const inputRef = useRef<ElementRef<"textarea">>(null)
+    const [isEditing, setIsEditing] = useState(false)
+    const [value, setValue] = useState(initialData.title)
+
+    const update = useMutation(api.document.update);
+
+    const enableInput = () => {
+        if(preview) return;
+
+        setIsEditing(true)
+        setTimeout(() => {
+            setValue(initialData.title);
+            inputRef.current?.focus();
+        } , 0)
+    };
+
+    const disabledInput = () => setIsEditing(false)
+
+    const onInput = (value : string) => {
+        setValue(value)
+        update({
+            id : initialData._id,
+            title : value || "Untitled"
+        })
+    }
+
+    const onKeyDown = (
+        event : React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if(event.key === "Enter"){
+            event.preventDefault();
+            disabledInput();
+        }
+    }
     return(
         <div
          className="pl-[54px] group relative">
@@ -58,7 +98,40 @@ export const Toolbar = ({
                         </IconPicker>
                     )
                 }
+                {
+                    !initialData.coverImage && !preview && 
+                    (
+                        <Button
+                        className="text-muted-foreground text-xs"
+                            variant="outline"
+                            size="sm"
+                            onClick={() => {}}
+                        >
+                            <ImageIcon className="h-4 w-4 mr-2"/>
+                            Add Cover
+                        </Button>
+                    )
+                }
             </div>
+
+            {
+                isEditing && !preview ? (
+                    <TextareaAutoSize
+                    ref={inputRef}
+                    onBlur={disabledInput}
+                    onKeyDown={onKeyDown}
+                    value={value}
+                    onChange={(e) => onInput(e.target.value)}
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+                    />
+                ) : (
+                    <div
+                        onClick={enableInput}
+                        className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+                        >
+                        </div>
+                )
+            }
         </div>
     )
 }

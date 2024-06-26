0ba99a63373a24e2b501d330688d97a344538c3d
mihir-n25 <mihirnebani@gmail.com>
2024-06-26 17:17:37 +0530

    toolbar component added

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -3,12 +3,41 @@
 import { useQuery } from "convex/react";
 
 import {api} from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel";
 
+interface DocumentIdPageProps {
+    params : {
+        documentId : Id<"documents">
+    }
+}
 
-const DocumentIdPage = () => {
+const DocumentIdPage = ({
+    params
+} : DocumentIdPageProps) => {
+
+    const document = useQuery(api.document.getById , {
+        documentId : params.documentId
+    })
+
+    if (document === undefined){
+        return (
+            <div>
+                Loading..
+            </div>
+        )
+    }
+    if (document === null){
+        return (
+            <div>
+                ummm..Not found
+            </div>
+        )
+    }
     return ( 
-        <div>
-            DocumentId
+        <div className="pb-40">
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <ToolBar initialData={document}/>
+            </div>
         </div>
      );
 }
